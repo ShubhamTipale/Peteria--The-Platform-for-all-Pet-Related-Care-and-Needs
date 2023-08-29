52d34d56c95ec443e1eead7d3cb866049361cf14
unknown <vaibhavmali2387@gmail.com>
2023-08-29 19:21:23 +0530

    abc

--- peteria/frontend_code/src/Component/customer/Breed.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Breed() {
-  return (
-    <div>
-      <h1>Breed List</h1>
-    </div>
-  )
-}
--- peteria/frontend_code/src/Component/customer/Vet.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Vet() {
-  return (
-    <div>
-      <h1>Vet List</h1>
-    </div>
-  )
-}
